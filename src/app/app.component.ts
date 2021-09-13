09fd406d8a1c25e506a36f7177243f8d664dda93
noisy86 <57344673+noisy86@users.noreply.github.com>
2021-09-14 01:32:12 +0200

    animationLearned

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+// import AOS from 'aos';
+import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'ds';
+  ngOnInit(){
+    AOS.init();
+  }
 }
