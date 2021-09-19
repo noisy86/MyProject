fa7cd6804a8e1f622f87c187091c064ee6223e4d
noisy86 <57344673+noisy86@users.noreply.github.com>
2021-09-19 21:13:02 +0200

    +

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { DownloadComponent } from './download/download.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DownloadComponent
   ],
   imports: [
     BrowserModule,
